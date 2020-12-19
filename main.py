f5585fdcc2b69b6d68443d460ce3cd10f2724dc9
daliveblaze <76107234+daliveblaze@users.noreply.github.com>
2020-12-19 22:34:53 +0000

    Added email functionality

--- main.py
+++ main.py
@@ -4,6 +4,33 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 
+
+import email, smtplib, ssl
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+subject = "An email with attachment from Python"
+body = "This is an email with attachment sent from Python"
+sender_email = "liveblaze@gmail.com"
+receiver_email = "dalconhai@gmail.com"
+password = "Q7ct29EEh@$YJD"
+
+
+# Create a multipart message and set headers
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message["Bcc"] = receiver_email  # Recommended for mass emails
+
+# Add body to email
+message.attach(MIMEText(body, "plain"))
+
+
 # initialize storage
 titles = []
 links = []
@@ -11,6 +38,26 @@ years = []
 roles = []
 filmo = []
 imdb_url = "https://www.imdb.com/"
+
+filepath = "mwf_acting.csv"
+
+# Open PDF file in binary mode
+with open(filepath, "rb") as attachment:
+    # Add file as application/octet-stream
+    # Email client can usually download this automatically as attachment
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+# Encode file in ASCII characters to send by email    
+encoders.encode_base64(part)
+
+# Add header as key/value pair to attachment part
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filepath}",
+)
+
+
 
 # make sure we get English-translated titles from all the movies we scrape:
 headers = {"Accept-Language" : "en-US, en;q=0.5"}
@@ -58,4 +105,31 @@ movies['year'] = movies['year'].str.strip()
 acting = movies.loc[movies['role'] == "Actor"]
 
 # Output the CSV
-acting.to_csv("~/mwf_acting.csv", index=False, encoding='utf-8')
+acting.to_csv(filepath, index=False, encoding='utf-8')
+
+
+# Open PDF file in binary mode
+with open(filepath, "rb") as attachment:
+    # Add file as application/octet-stream
+    # Email client can usually download this automatically as attachment
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+# Encode file in ASCII characters to send by email    
+encoders.encode_base64(part)
+
+# Add header as key/value pair to attachment part
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filepath}",
+)
+
+# Add attachment to message and convert message to string
+message.attach(part)
+text = message.as_string()
+
+# Log in to server using secure context and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, text)
